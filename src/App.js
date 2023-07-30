54fb5f90cae6aa756df8cfa3be88f410893347d3
Fatima Zahra <fathima_z@yahoo.com>
2023-07-30 21:42:21 +0100

    Added our own API Key+completed installations

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-
   const [recipe, setRecipe] = useState();
-
 
   // Link to Spoonacular Search API Documentation: https://spoonacular.com/food-api/docs#Get-Random-Recipes
   async function getRandomRecipe() {
     try {
       //IMPORTANT! Update the below variable with your own api key!!
-      const apiKey = '';
+      const apiKey = "f03b5fc1db7a40a5bff306a20c50d54a";
 
       //making spoonacular api call to get a random recipe
-      let resp = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`);
+      let resp = await axios.get(
+        `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`
+      );
       console.log(21, resp.data);
 
       //store the random recipe into the recipe variable
@@ -23,59 +22,44 @@ function App() {
     } catch (e) {
       console.log(e);
     }
-
   }
 
   useEffect(() => {
     getRandomRecipe();
   }, []);
 
-
-
   return (
-
     <div className="row">
-
-      <button onClick={getRandomRecipe}>
-        Generate Random Recipie
-      </button>
-
+      <button onClick={getRandomRecipe}>Generate Random Recipie</button>
 
       <div>
         Name:
         <a target="_blank" href={recipe?.sourceUrl}>
           {recipe?.title}
-        </a>2
+        </a>
+        2
       </div>
       <img src={recipe?.image} />
 
       <div className="ingredients">
-        <div>
-          Ingredients needed:
-        </div>
-        {recipe?.extendedIngredients.map((ingredient, index) =>
+        <div>Ingredients needed:</div>
+        {recipe?.extendedIngredients.map((ingredient, index) => (
           <span key={index}>
-
-            {index != recipe?.extendedIngredients.length - 1 ? ingredient.name + ", " : ingredient.name}
+            {index != recipe?.extendedIngredients.length - 1
+              ? ingredient.name + ", "
+              : ingredient.name}
           </span>
-        )}
-        {recipe?.analyzedInstructions.map((instruction) =>
+        ))}
+        {recipe?.analyzedInstructions.map((instruction) => (
           <ol>
-            {instruction.steps?.map((step) =>
-              <li>
-                {step.step}
-              </li>
-            )}
+            {instruction.steps?.map((step) => (
+              <li>{step.step}</li>
+            ))}
           </ol>
-        )
-        }
-      </div >
-      <div>
-
+        ))}
       </div>
-
-    </div >
-
+      <div></div>
+    </div>
   );
 }
 
